01852a4393427a19d452ed78ba92cb73422733a9
Chetan Padia <chetbox@users.noreply.github.com>
2017-11-15 16:06:28 +0000

    Fix SimpleDateFormat crash on pre-Android Nougat devices
    Resolves #1

--- lib/src/main/src/co/beeline/gpx/Time.kt
+++ lib/src/main/src/co/beeline/gpx/Time.kt
@@ -3,6 +3,7 @@ package co.beeline.gpx
 import co.beeline.gpx.xml.XmlWritable
 import co.beeline.gpx.xml.XmlWrite
 import rx.Observable
+import java.text.FieldPosition
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -12,8 +13,19 @@ import java.util.*
 data class Time(val date: Long?) : XmlWritable {
 
     companion object {
-        private val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX", Locale.ENGLISH).apply {
-            timeZone = TimeZone.getTimeZone("UTC")
+        private val dateFormat = object : SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.ENGLISH) {
+
+            init {
+                timeZone = TimeZone.getTimeZone("UTC")
+            }
+
+            // Workaround to generate a 'Z' at the end of the string to denote UTC
+            // which does not rely on the 'X' format string character (only available from Android 24)
+            // See: https://developer.android.com/reference/java/text/SimpleDateFormat.html
+            override fun format(date: Date?, toAppendTo: StringBuffer?, pos: FieldPosition?): StringBuffer {
+                return super.format(date, toAppendTo, pos).append('Z')
+            }
+
         }
     }
 
