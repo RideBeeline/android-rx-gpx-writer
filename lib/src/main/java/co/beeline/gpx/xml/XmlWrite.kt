5408af201e0f3797da2c2092fc050be188bd685d
Marc Baldwin <marc.baldwin88@gmail.com>
2019-12-16 15:40:55 +0000

    Update to RxJava2

--- lib/src/main/java/co/beeline/gpx/xml/XmlWrite.kt
+++ lib/src/main/java/co/beeline/gpx/xml/XmlWrite.kt
@@ -1,27 +1,27 @@
 package co.beeline.gpx.xml
 
 import android.util.Xml
+import io.reactivex.Observable
+import io.reactivex.Single
 import org.xmlpull.v1.XmlSerializer
-import rx.Observable
-import rx.Single
 import java.io.Writer
 import java.nio.charset.Charset
 import java.nio.charset.StandardCharsets
 
 typealias XmlWrite = (XmlSerializer) -> Unit
 
-fun Observable<XmlWrite>.writeTo(writer: Writer, charset: Charset = StandardCharsets.UTF_8): Single<Writer> =
-        startWith { xml ->
-            xml.setOutput(writer)
-            return@startWith xml.startDocument(charset.name(), null)
-        }
-                .concatWith(Observable.just { xml ->
-                    xml.endDocument()
-                    return@just writer.close()
-                })
-                .reduce(
+fun Observable<XmlWrite>.writeTo(writer: Writer, charset: Charset = StandardCharsets.UTF_8): Single<Writer> {
+    return startWith { xml ->
+        xml.setOutput(writer)
+        xml.startDocument(charset.name(), null)
+    }
+            .concatWith(Observable.just { xml ->
+                xml.endDocument()
+                writer.close()
+            })
+            .reduce(
                     Xml.newSerializer().apply { setFeature("http://xmlpull.org/v1/doc/features.html#indent-output", true) },
                     { xml, write -> xml.apply { write(this) } }
-                )
-                .map { writer }
-                .toSingle()
+            )
+            .map { writer }
+}
--- lib/src/test/java/co/beeline/gpx/GpxTest.kt
+++ lib/src/test/java/co/beeline/gpx/GpxTest.kt
@@ -1,31 +1,31 @@
 package co.beeline.gpx
 
+import io.reactivex.Observable
 import org.apache.tools.ant.filters.StringInputStream
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
 import org.xmlunit.builder.DiffBuilder
 import org.xmlunit.builder.Input
 import org.xmlunit.validation.Languages
 import org.xmlunit.validation.Validator
-import rx.Observable
 import java.io.File
 import java.io.StringWriter
 import javax.xml.transform.stream.StreamSource
 
 @RunWith(RobolectricTestRunner::class)
-@Config(constants = BuildConfig::class)
 class GpxTest {
 
-    @Test fun empty() = assertGpxEquals(fixture("empty.gpx"), Gpx(creator = "empty"))
+    @Test
+    fun empty() = assertGpxEquals(fixture("empty.gpx"), Gpx(creator = "empty"))
 
-    @Test fun waypoints() = assertGpxEquals(fixture("waypoints.gpx"), Gpx(
+    @Test
+    fun waypoints() = assertGpxEquals(fixture("waypoints.gpx"), Gpx(
             creator = "RouteConverter",
             metadata = Metadata(name = "Test file by Patrick"),
-            waypoints = list(
+            waypoints = Observable.fromArray(
                     Waypoint(54.9328621088893, 9.860624216140083, name = "Position 1", ele = 0.0),
                     Waypoint(54.93293237320851, 9.86092208681491, name = "Position 2", ele = 0.0),
                     Waypoint(54.93327743521187, 9.86187816543752, name = "Position 3", ele = 0.0),
@@ -33,50 +33,41 @@ class GpxTest {
             )
     ))
 
-    @Test fun track() = assertGpxEquals(fixture("track.gpx"), Gpx(
+    @Test
+    fun track() = assertGpxEquals(fixture("track.gpx"), Gpx(
             creator = "RouteConverter",
             metadata = Metadata(name = "Test file by Patrick"),
-            tracks = list(Track(
-                    name = "Patrick's Track",
-                    segments = list(TrackSegment(list(
-                            TrackPoint(54.9328621088893, 9.860624216140083, name = "Position 1", ele = 0.0, time = 1505900660000),
-                            TrackPoint(54.93293237320851, 9.86092208681491, name = "Position 2", ele = 0.0, time = 1505900661000),
-                            TrackPoint(54.93327743521187, 9.86187816543752, name = "Position 3", ele = 0.0, time = 1505900662000),
-                            TrackPoint(54.93342326167919, 9.862439849679859, name = "Position 4", ele = 0.0, time = 1505900663000)
-                    )))
-            ))
+            tracks = Observable.just(
+                    Track(
+                            name = "Patrick's Track",
+                            segments = Observable.just(
+                                    TrackSegment(Observable.fromArray(
+                                            TrackPoint(54.9328621088893, 9.860624216140083, name = "Position 1", ele = 0.0, time = 1505900660000),
+                                            TrackPoint(54.93293237320851, 9.86092208681491, name = "Position 2", ele = 0.0, time = 1505900661000),
+                                            TrackPoint(54.93327743521187, 9.86187816543752, name = "Position 3", ele = 0.0, time = 1505900662000),
+                                            TrackPoint(54.93342326167919, 9.862439849679859, name = "Position 4", ele = 0.0, time = 1505900663000)
+                                    )))
+                    ))
     ))
 
-    @Test fun route() = assertGpxEquals(fixture("route.gpx"), Gpx(
+    @Test
+    fun route() = assertGpxEquals(fixture("route.gpx"), Gpx(
             creator = "RouteConverter",
             metadata = Metadata(name = "Test file by Patrick"),
-            routes = list(Route(
-                    name = "Patrick's Route",
-                    points = list(
-                            RoutePoint(54.9328621088893, 9.860624216140083, name = "Position 1", ele = 0.0),
-                            RoutePoint(54.93293237320851, 9.86092208681491, name = "Position 2", ele = 0.0),
-                            RoutePoint(54.93327743521187, 9.86187816543752, name = "Position 3", ele = 0.0),
-                            RoutePoint(54.93342326167919, 9.862439849679859, name = "Position 4", ele = 0.0)
+            routes = Observable.just(
+                    Route(
+                            name = "Patrick's Route",
+                            points = Observable.fromArray(
+                                    RoutePoint(54.9328621088893, 9.860624216140083, name = "Position 1", ele = 0.0),
+                                    RoutePoint(54.93293237320851, 9.86092208681491, name = "Position 2", ele = 0.0),
+                                    RoutePoint(54.93327743521187, 9.86187816543752, name = "Position 3", ele = 0.0),
+                                    RoutePoint(54.93342326167919, 9.862439849679859, name = "Position 4", ele = 0.0)
+                            )
                     )
-            ))
+            )
     ))
-
-    private fun <T> list(vararg items: T): Observable<T> = Observable.from(listOf(*items))
 
     private fun fixture(filename: String): File = File("fixtures", filename)
-
-    private fun Gpx.xmlString(): String =
-        writeTo(StringWriter())
-                .toBlocking()
-                .value()
-                .toString()
-
-    private fun assertValidGpx(xml: String) {
-        Validator.forLanguage(Languages.W3C_XML_SCHEMA_NS_URI)
-                .apply { setSchemaSource(StreamSource(fixture("GPXv1.1.xsd"))) }
-                .validateInstance(StreamSource(StringInputStream(xml)))
-                .let { assertTrue(it.problems.joinToString("\n") + "\n---\n" + xml, it.isValid) }
-    }
 
     private fun assertGpxEquals(expectedFile: File, actual: Gpx) {
         DiffBuilder.compare(Input.fromFile(expectedFile))
@@ -88,4 +79,15 @@ class GpxTest {
         assertValidGpx(actual.xmlString())
     }
 
+    private fun assertValidGpx(xml: String) {
+        Validator.forLanguage(Languages.W3C_XML_SCHEMA_NS_URI)
+                .apply { setSchemaSource(StreamSource(fixture("GPXv1.1.xsd"))) }
+                .validateInstance(StreamSource(StringInputStream(xml)))
+                .let { assertTrue(it.problems.joinToString("\n") + "\n---\n" + xml, it.isValid) }
+    }
+
+    private fun Gpx.xmlString(): String =
+            writeTo(StringWriter())
+                    .blockingGet()
+                    .toString()
 }
